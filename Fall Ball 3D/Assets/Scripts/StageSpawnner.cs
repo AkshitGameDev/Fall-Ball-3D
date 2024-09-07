0632f0c55827638a61168a61ca01e94dedd1d50d
Akshit Sharma <akshitsharma196@gmail.com>
2024-09-07 01:21:15 -0400

    stage spawner in progress
    spawning blocks

--- Fall Ball 3D/Assets/Scripts/StageSpawnner.cs
+++ Fall Ball 3D/Assets/Scripts/StageSpawnner.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 
 public class StageSpawnner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static StageSpawnner instance;
+
+    public int TotalblocksSpawnnedInTower = 0;
+
+    public GameObject[] blocksList;
+
+
+    // Start is called before the first frame updat
+    void Awake()
     {
-        
+        instance = this;
+    }
+
+    public void getCredentials(int blocks)
+    {
+        Debug.Log("number of blocks" + blocks);
+        TotalblocksSpawnnedInTower = blocks;
+
+
+
+    }
+
+    void SpawnInitialBlock()
+    {
+
+    }
+
+
+    void SpawnTowerBlocks()
+    {
+
     }
 
     // Update is called once per frame
